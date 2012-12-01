2a87bc63e8663244992e6da1ba58bd910098b12c
gorostas <kpendic@gmail.com>
2012-12-01 23:35:37 +0100

    clean start + config

--- php/wp-config.php
+++ php/wp-config.php
@@ -46,50 +46,7 @@ define('DB_COLLATE', '');
 // This is where we define the OpenShift specific secure variable functions
 require_once(ABSPATH . '/wp-includes/openshift.inc');
 
-// Set the default keys to use
-$_default_keys = array(
-  'AUTH_KEY'          => ' w*lE&r=t-;!|rhdx5}vlF+b=+D>a)R:nTY1Kdrw[~1,xDQS]L&PA%uyZ2:w6#ec',
-  'SECURE_AUTH_KEY'   => '}Sd%ePgS5R[KwDxdBt56(DM:0m1^4)-k6_p8}|C:[-ei:&qA)j!X`:7d-krLZM*5',
-  'LOGGED_IN_KEY'     => '$l^J?o)!zhp6s[-x^ckF}|BjU4d+(g1as)n/Q^s+k|,ZZc@E^h%Rx@VTm|0|?]6R',
-  'NONCE_KEY'         => '#f^JM8d^!sVsq]~|4flCZHdaTy.-I.f+1tc[!h?%-+]U}|_8qc K=k;]mXePl-4v',
-  'AUTH_SALT'         => 'I_wL2t!|mSw_z_ zyIY:q6{IHw:R1yTPAO^%!5,*bF5^VX`5aO4]D=mtu~6]d}K?',
-  'SECURE_AUTH_SALT'  => '&%j?6!d<3IR%L[@iz=^OH!oHRXs4W|D,VCD7w%TC.uUa`NpOH_XXpGtL$A]{+pv9',
-  'LOGGED_IN_SALT'    => 'N<mft[~OZp0&Sn#t(IK2px0{KloRcjvIJ1+]:,Ye]>tb*_aM8P&2-bU~_Z>L/n(k',
-  'NONCE_SALT'        => 'u E-DQw%[k7l8SX=fsAVT@|_U/~_CUZesq{v(=y2}#X&lTRL{uOVzw6b!]`frTQ|'
-);
 
-// This function gets called by openshift_secure and passes an array
-function make_secure_key($args) {
-  $hash = $args['hash'];
-  $key  = $args['variable'];
-  $original = $args['original'];
-
-	$chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
-  $chars .= '!@#$%^&*()';
-  $chars .= '-_ []{}<>~`+=,.;:/?|';
-
-  // Convert the hash to an int to seed the RNG
-  srand(hexdec(substr($hash,0,8)));
-  // Create a random string the same length as the default
-  $val = '';
-  for($i = 1; $i <= strlen($original); $i++){
-    $val .= substr( $chars, rand(0,strlen($chars))-1, 1);
-  }
-  // Reset the RNG
-  srand();
-  // Set the value
-  return $val;
-}
-
-// Generate OpenShift secure keys (or return defaults if not on OpenShift)
-$array = openshift_secure($_default_keys,'make_secure_key');
-
-// Loop through returned values and define them
-foreach ($array as $key => $value) {
-  define($key,$value);
-}
-
-/**#@-*/
 
 /**
  * WordPress Database Table prefix.
@@ -108,6 +65,18 @@ $table_prefix  = 'wp_';
  * language support.
  */
 define('WPLANG', '');
+
+define('WP_CACHE', true);
+
+# define('WP_ALLOW_MULTISITE', false);
+define('MULTISITE', true);
+define('SUBDOMAIN_INSTALL', false);
+# $base = '/';
+# define('DOMAIN_CURRENT_SITE', 'mkdizajn.com');
+# define('PATH_CURRENT_SITE', '/');
+# define('SITE_ID_CURRENT_SITE', 1);
+# define('BLOG_ID_CURRENT_SITE', 1);
+
 
 /**
  * For developers: WordPress debugging mode.
